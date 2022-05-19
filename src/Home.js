b879cc3a34c21f44f60238869613f04ef5059cc7
Zack Davis <jantrezac95@gmail.com>
2022-05-19 15:10:17 -0500

    set up routing for pages

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home(){
+    return(
+        <div>
+            <h1>This is where the matching will happen</h1>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/Liked.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Liked(){
+    return(
+        <div>
+            <h1>This is where you can see those you liked</h1>
+        </div>
+    )
+}
+
+export default Liked;
